9edbb6ea0701c47d61c83c294b8190438b1ef37d
sloppie <dgitau75@gmail.com>
2020-05-01 16:10:39 -0400

    Carrying out error calculation in the neural network

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ class NN:
     def __init__(self, layers_arr):
         self.weights = []
         self.biases = []
+        self.errors = np.zeros([layers_arr[-1], 1])
         i = 0
 
         for value in layers_arr:
@@ -19,7 +20,7 @@ class NN:
         print("Weights size: " + str(len(self.weights)))
         print("Bias size: " + str(self.biases[0].shape))
 
-    def feed_forward(self, inputs):
+    def feed_forward(self, inputs, expected_output):
         inputted = False
         i = 0
         z = [] # This is the raw (w.a + b)
@@ -29,19 +30,23 @@ class NN:
 
             if(not inputted):
                 z.append(np.matmul(weight, inputs) + self.biases[i])
-                print("Before activation: ")
-                print(z[-1])
                 a.append(1.0 / (1.0 + np.exp(-z[-1])))
                 inputted = True
-                print("Fed Forward: ")
-                print(a[-1])
             else:
                 z.append(np.matmul(weight, a[-1]) + self.biases[i])
                 a.append(1.0 / (1.0 + np.exp(-z[-1])))
-                print("Fed Forward: ")
-                print(a[-1])
 
             i = i + 1
 
+        self.calculate_error(a[-1], expected_output)
+        # return [z, a]
+
+    def calculate_error(self, a, expected_output):
+        self.errors += expected_output - a[-1]
+
+# Initialise the Neural network
 example_net = NN([784, 16, 10])
-example_net.feed_forward(np.random.rand(784, 1))
+expected_output = np.zeros((10, 1))
+expected_output[3, 0] = 1.0
+example_net.feed_forward(np.random.rand(784, 1), expected_output)
+print("error: \n" + str(example_net.errors))
